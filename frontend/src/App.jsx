41b07de32c92dee2da1ce03367a85bea297a9b14
dainguyenn <dnguyennguyenn11@gmail.com>
2023-05-13 22:50:56 +0700

    Config format

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <h1>Init</h1>
-      <h2>Welcome</h2>
-    </>
-  )
+    return (
+        <>
+            <h1>Init</h1>
+            <h2>Welcome</h2>
+        </>
+    );
 }
 
 export default App;
