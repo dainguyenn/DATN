bdb81d91d10b0924298d2b2ae721aa1caee7ccdb
donglv <donglv@newwave.vn>
2023-06-12 15:14:53 +0700

    feat: pay.php

--- guixe/View/vehicle/taking/pay.php
+++ guixe/View/vehicle/taking/pay.php
@@ -69,7 +69,7 @@
         //print_r($priceLevel);
     } else {
         echo "<h1>Loại xe là xe máy à </h1>";
-        $startTime = new DateTime('2023-06-05 12:00:00');
+        $startTime = new DateTime($luotGui["gio_vao"]);
         $endTimeStr = date("Y-m-d H:i:s");
         $endTime = new DateTime($endTimeStr);
         // Giá tiền ban ngày
@@ -81,13 +81,108 @@
         $dayTotal = 0;
         $nightToTal = 0;
         print_r($timeSpan);
-        if($startTime->format("d") != $endTime->format("d")){
+        // gửi trong nhiều ngày
+        if ($startTime->format("d") != $endTime->format("d")) {
             //tiền ngày gửi đầu
-            if($startTime->format("H"))
+            if ($startTime->format("H") < 6) {
+                $dayTotal += 1; //1 ca ngày
+                $nightToTal += 2; //2 ca đêm
+                echo "<h1>A1</h1>";
+            }
+            if ($startTime->format("H") >= 6 && $startTime->format("H") < 18) {
+                $dayTotal += 1; //1 ca ngày
+                $nightToTal += 1; // 1 ca đêm
+                echo "<h1>A2</h1>";
+            }
+            if ($startTime->format("H") >= 18) {
+                $nightToTal = $nightToTal + 1; // 1 ca đêm
+                echo "<h1>A3 ${nightToTal}</h1>";
+            }
+
+            //tiền gửi ngày giữa
+            $totalDays = $endTime->format("d") - $startTime->format("d");
+            //$totalPrice += ($totalDays - 1) * ($dayPrice);
+            if ($totalDays >= 2) {
+                $dayTotal += ($totalDays - 1);
+                $nightToTal += ($totalDays - 1) * 2 - 1;
+            }
+            echo "<h1>B1</h1>";
+            //tiền gửi ngày cuối
+            if ($endTime->format("H") < 6) { //
+                $nightToTal = $nightToTal + 1; //1 ca đêm
+                echo "<h1>C1</h1>";
+            }
+            if ($endTime->format("H") >= 6 && $startTime->format("H") < 18) {
+                $dayTotal += 1; //1 ca ngày
+                $nightToTal += 1; // 1 ca đêm
+                echo "<h1>C2</h1>";
+            }
+            if ($endTime->format("H") >= 18) {
+                $dayTotal += 1; //1 ca ngày
+                $nightToTal = $nightToTal + 1; //2 ca đêm
+                echo "<h1>C3 ${nightToTal}</h1>";
+            }
+        } else {
+            if ($startTime->format("H") < 6) {
+                $dayTotal += 1; //1 ca ngày
+                $nightToTal += 2; //2 ca đêm
+                echo "<h1>A1</h1>";
+            }
+            if ($startTime->format("H") >= 6 && $startTime->format("H") < 18) {
+                $dayTotal += 1; //1 ca ngày
+                $nightToTal += 1; // 1 ca đêm
+                echo "<h1>A2</h1>";
+            }
+            if ($startTime->format("H") >= 18) {
+                $nightToTal = $nightToTal + 1; // 1 ca đêm
+                echo "<h1>A3 ${nightToTal}</h1>";
+            }
+
         }
+        echo <<<LOG
+            <table>
+            <tr> 
+                <td>Số mức tính ban ngày</td>
+                <td>Số mức tính ban đêm</td>
+            </tr>
+            <tr> 
+                <td>${dayTotal}</td>
+                <td>${nightToTal}</td>
+            </tr>
+            </table>
+        LOG;
+        $thanhTien = $dayTotal * $dayPrice + $nightToTal * $nightPrice;
+        $result_str_show = <<<result
+        <table border='1' cellpadding='5' cellspacing='0'>
+        <tr>
+            <td>Thời gian bắt đầu gửi</td>
+            <td>${luotGui['gio_vao']}</td>
+        </tr>
+        <tr>
+            <td>Thời gian kết thúc gửi</td>
+            <td>${endTimeStr}</td>
+        </tr>
+        <tr>
+            <td>Số lần gửi xe ban ngày </td>
+            <td>${$dayTotal}</td>
+        </tr>
+        <tr>
+            <td>Số lần gửi xe ban đêm </td>
+            <td>${nightToTal}</td>
+        </tr>
+        <tr>
+            <td>Thành tiền </td>
+            <td>${thanhTien}</td>
+        </tr>
+        </table>
+        result;
+        echo $result_str_show;
     }
 
-
+    if (isset($_POST["sub"])) {
+        $_SESSION["da_thanh_toan"] = true;
+        echo "<script>window.location.href = 'GhiNhanThongTin.php'</script>";
+    }
     ?>
 </body>
 
