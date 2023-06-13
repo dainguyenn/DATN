381b2b6a487af10d7aa45eb45715ea3d560b271a
donglv <donglv@newwave.vn>
2023-06-13 21:01:43 +0700

    fix: ignore

---
+++ guixe/Constant/EnvConst.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Constant;
+
+class EnvConst
+{
+    public const DB_CONNECTION = 'mysql';
+    public const DB_HOST = 'localhost';
+    public const DB_PORT = '3306';
+    public const DB_DATABASE = 'guixe';
+    public const DB_USERNAME = 'root';
+    public const DB_PASSWORD = '';
+    public const SQL_LOG_PATH = '/mnt/d/DATA/DATN/Project/guixe';
+    public const USE_SQL_LOG = true;
+
+}
