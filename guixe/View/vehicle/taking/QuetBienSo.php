6fd47ffdcf3643126df9ed8a528fd56e3521e0f4
levandong1503 <92266441+levandong1503@users.noreply.github.com>
2023-06-13 20:58:01 +0700

    caculator parking fee (#24)
    * feat: fakeData in `chuHo` and generate tables
    
    * fix: come back create table
    
    * feat: parking and taking vehicle
    
    * fix: insert vehicle number when parking
    
    * fix: insert vehicle number when parking
    
    * feat: pay.php
    
    * feat caculator parking fee
    
    ---------
    
    Co-authored-by: donglv <donglv@newwave.vn>

--- guixe/View/vehicle/taking/QuetBienSo.php
+++ guixe/View/vehicle/taking/QuetBienSo.php
@@ -20,22 +20,24 @@
     session_start();
     $veModel = new \Model\Ve();
     $thongTinVeModel = new \Model\ThongTinVe();
-    $ve = $_SESSION["ve"];
-    $luotGui = $_SESSION["luot_gui"];
-    if (!isset($ve) || !isset($luotGui))
+    $luotGuiModel = new \Model\LuotGui();
+    $ve = $_SESSION["ve_lay"];
+    $laDangGui = $_SESSION["la_dang_gui"];
+    if (!isset($ve) || !isset($laDangGui) || !$laDangGui)
         header("location:index.php");
-    print_r($_SESSION);
+    $luotGui = $luotGuiModel->GetThongTinTheDangGui($ve["ma_ve"]);
+    //print_r($_SESSION);
     if (isset($_POST["sub"])) {
         if ($ve["loai_the"] == "Tháng") {
             $result = $thongTinVeModel->findById($ve["ma_ve"])[0];
-            print_r($result);
+            //print_r($result);
             if (!$result) {
                 echo "lỗi trong quá trình kiểm tra";
                 exit;
             }
             if ($_POST["bien_so_xe"] == $result["bien_so_xe"]) {
                 echo "xác nhận biển số thành công";
-                $_SESSION["thong_tin_ve"] = $result;
+                $_SESSION["xac_nhan_bien_so_lay"] = true;
                 echo "<script>window.location.href = 'GhiNhanThongTin.php'</script>";
             } else {
                 echo "xác nhận biến số không hợp lệ";
@@ -43,8 +45,9 @@
             }
         } else {
             if ($luotGui["bien_so_xe"] == $_POST["bien_so_xe"]) {
-                echo "<h1>Là thẻ ngày</h1> <h2>Đang ghi nhận thông tin lấy</h2>";
-                echo "<script>window.location.href = 'GhiNhanThongTin.php'</script>";
+                echo "<h1>Là thẻ ngày</h1> <h2>Tiến hành thanh toán</h2>";
+                $_SESSION["xac_nhan_bien_so_lay"] = true;
+                echo "<script>window.location.href = 'pay.php'</script>";
             } else {
                 echo "<h1>Biển số xe không trùng khớp</h1>";
             }
