a3afbd5cd8d0ceefd7e6ab944d1b754f99469945
dainguyenn <109606524+dainguyenn@users.noreply.github.com>
2023-06-15 20:46:35 +0700

    Dainguyen (#26)
    * Fix basemodele
    
    * Session helper
    
    * abc
    
    * Base
    
    ---------
    
    Co-authored-by: dainguyenn <dnguyennguyenn11@gmail.com>
    Co-authored-by: unknown <BinhBui2412>

--- guixe/View/sidebar-header.php
+++ guixe/View/sidebar-header.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../Css/Sidebar-header.css">
-    <link href="/css/fontawesome-free-6.2.0-web/css/all.css" rel="stylesheet" ></link>
+    <link rel="stylesheet" href="/Css/Sidebar-header.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <title></title>
 </head>
@@ -98,6 +97,11 @@
                     </div>  
                 </li>
             </nav>
+        </div>
+        <div class="main-panel">
+            <div class="content-wrapper">
+                {{content}}
+            </div>
         </div>
     </div>
 </body>
