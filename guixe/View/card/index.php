ad1c034997b3653b2c1c2e2e0d8c774ead08b327
levandong1503 <92266441+levandong1503@users.noreply.github.com>
2023-06-21 20:50:04 +0700

    table (#32)
    * feat: fakeData in `chuHo` and generate tables
    
    * fix: come back create table
    
    * feat: parking and taking vehicle
    
    * fix: insert vehicle number when parking
    
    * fix: insert vehicle number when parking
    
    * feat: pay.php
    
    * feat caculator parking fee
    
    * fix: ignore
    
    * fix: layout; feat: `bang_gia` table
    
    * style table
    
    * ignore
    
    * ignore
    
    * ignore
    
    * ignore
    
    ---------
    
    Co-authored-by: donglv <donglv@newwave.vn>

--- guixe/View/card/index.php
+++ guixe/View/card/index.php
@@ -4,13 +4,24 @@ require_once '../../autoload.php';
 $veModel = new \Model\Ve();
 $page = $_GET['page'] ?? 1;
 $limit = $_GET['limit'] ?? 25;
-$allVe = $veModel->getInfo(null,['*'],$limit,$page);
+$allVe = $veModel->getInfo(null, ['*'], $limit, $page);
 ?>
 <!--Luôn import-->
 <?php ob_start(); ?>
 <!---->
 <div>
     <a href="createMonthCard.php">Thêm</a>
+<<<<<<< HEAD
+    <ul class="responsive-table">
+        <li class="table-header">
+            <div class="col col-1">Mã vé</div>
+            <div class="col col-1">Mã căn hộ</div>
+            <div class="col col-2">Tên chủ hộ</div>
+            <div class="col col-1">Biển số xe</div>
+            <div class="col col-1">Loại thẻ</div>
+            <div class="col col-1">Loại xe</div>
+        </li>
+=======
     <table>
         <thead>
         <tr>
@@ -23,10 +34,33 @@ $allVe = $veModel->getInfo(null,['*'],$limit,$page);
             <th></th>
         </tr>
         </thead>
+>>>>>>> 523e58589a6a9f192dc2be2b8ece0bfc71611e6d
         <?php
         foreach ($allVe['data'] as $item) {
 
             ?>
+<<<<<<< HEAD
+            <li class="table-row">
+                <div class="col col-1">
+                    <?php echo $item['ma_ve'] ?>
+                </div>
+                <div class="col col-1">
+                    <?php echo $item['ma_can_ho'] ?>
+                </div>
+                <div class="col col-2">
+                    <?php echo $item['ten_chu_ho'] ?>
+                </div>
+                <div class="col col-1">
+                    <?php echo $item['bien_so_xe'] ?>
+                </div>
+                <div class="col col-1">
+                    <?php echo $item['loai_ve'] ?>
+                </div>
+                <div class="col col-1">
+                    <?php echo $item['loai_xe'] ?>
+                </div>
+            </li>
+=======
             <tr>
                 <td><?php echo $item['ma_ve']?></td>
                 <td><?php echo $item['ma_can_ho']?></td>
@@ -39,8 +73,9 @@ $allVe = $veModel->getInfo(null,['*'],$limit,$page);
                     <button>Sửa</button>
                 </td>
             </tr>
+>>>>>>> 523e58589a6a9f192dc2be2b8ece0bfc71611e6d
         <?php } ?>
-    </table>
+    </ul>
 </div>
 <!--Luôn import (coppy vào file của mình)-->
 <?php $content = ob_get_clean(); ?>
